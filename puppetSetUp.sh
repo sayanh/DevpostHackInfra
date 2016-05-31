1fef2a5b79224e6113dd6f1735286e74ac067e03
Sayan Hazra <sayan.hazra@flixbus.de>
2016-05-31 08:37:29 +0200

    Added puppet set up script

---
+++ puppetSetUp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y python-software-properties
+cd /tmp && wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb && sudo dpkg -i puppetlabs-release-precise.deb
+sudo apt-get update 
+sudo apt-get install -y puppet-common
+sudo apt-get install -y puppet
+
+puppet module install garethr/docker
+puppet module install saz-sudo
+puppet module install puppetlabs/vcsrepo
+
+sudo puppet apply --modulepath=/vagrant/modules:/home/vagrant/.puppet/modules /vagrant/manifests/site.pp
+
+cd ~
+# sudo gpasswd -a ${USER} docker
+# sudo service docker restart
+# alias docker='sudo docker'
+
+
